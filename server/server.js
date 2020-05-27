e00f0449ff31014c7fc20b50cc4ef3b8f1695f48
Manank Valand <manank@Mananks-MacBook-Pro.local>
2020-05-27 00:35:31 -0400

    added cors

--- server/server.js
+++ server/server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const colors = require('colors');
 const mongoose = require('mongoose');
+const cors = require("cors");
 const configRoutes = require("./routes");
 const config = require('./config');
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
